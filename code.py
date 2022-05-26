8537e4aab12961810e7a81a6dc4f65b8c87188e0
lavu7592 <lavu7592@gmail.com>
2022-05-26 18:29:30 +0530

    new project hello world in code.py

--- code.py
+++ code.py
@@ -1,12 +1,1 @@
-def recur_fibo(a):
-   if a <= 1:
-       return a
-   else:
-       return(recur_fibo(a-1) + recur_fibo(a-2))
-aterms = 12
-if aterms <= 0:
-   print("Plese enter a positive integer")
-else:
-   print("Fibonacci sequence:")
-   for i in range(aterms):
-    
+prinmt("hello world")
