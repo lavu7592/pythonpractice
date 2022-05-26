43268f8d86da19f8dffb3c98b4e39e1c2b882aa7
lavu7592 <lavu7592@gmail.com>
2022-05-26 18:31:16 +0530

    old+updated project

--- code.py
+++ code.py
@@ -1,1 +1,18 @@
+<<<<<<< Updated upstream
 prinmt("hello world")
+=======
+def recur_fibo(a):
+   if a <= 1:
+       return a
+   else:
+       return(recur_fibo(a-1) + recur_fibo(a-2))
+aterms = 12
+if aterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(aterms):
+       if i=0:
+           print("hello world")
+    
+>>>>>>> Stashed changes
